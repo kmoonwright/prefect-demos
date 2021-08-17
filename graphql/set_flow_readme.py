582e236348c0dd7ed0d88fe1a6d122750a79f4f4
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-08-17 14:23:00 -0700

    Add mutation to add README to a flow programmatically

---
+++ graphql/set_flow_readme.py
@@ -0,0 +1,20 @@
+# ATM Setting a Flow ReadME is only available via GQL
+
+mutation = """
+mutation($input: set_flow_group_description_input!) {
+   set_flow_group_description(input: $input) {
+      success
+   } 
+}
+"""
+
+from prefect import Client
+
+client = Client()
+
+client.graphql(
+    mutation,
+    variables=dict(
+        input=dict(description="<README_STRING>", flow_group_id="<FLOW_GROUP_ID>")
+    ),
+)
