9970b9d87ddae752b01f2b649936fc50183f0de1
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-08-17 14:10:54 -0700

    Add annotations

--- core/evolving_etl_2/level_3.py
+++ core/evolving_etl_2/level_3.py
@@ -4,12 +4,14 @@ from prefect import task, Flow, Parameter, case, context
 from prefect.engine.results import LocalResult
 from prefect.triggers import all_successful, any_failed, some_successful
 
+# Custom state handler for task logging
 def log_state(task, old_state, new_state):
     if new_state.is_successful():
         logger = context.get("logger")
         logger.info(f"\nTask {task} finished in state: {new_state}\n")
     return new_state
 
+# Task definitions with Functional API, adding task level configurations
 length = Parameter(name="length", default=3)
 
 @task(max_retries=3, retry_delay=timedelta(seconds=5), state_handlers=[log_state])
@@ -24,6 +26,7 @@ def transform(data):
 def load(data):
     print(f"\nHere's your data: {data}")
 
+# Schedule object consisting of two clocks
 from prefect.schedules import Schedule
 from prefect.schedules.clocks import IntervalClock
 
@@ -40,7 +43,9 @@ clock2 = IntervalClock(
 
 schedule = Schedule(clocks=[clock1, clock2])
 
+# Flow Definition for task dependencies in with block
 with Flow("Evolving ETL", schedule=schedule) as flow:
+    # Case blocks for our Parameter task
     with case(length, 6):
         e = extract(length)
         t = transform.map(e)
@@ -51,3 +56,6 @@ with Flow("Evolving ETL", schedule=schedule) as flow:
         t = transform.map(e)
         t2 = transform.map(t)
         l = load(t2)
+
+# Rather than flow.run(), utilize the CLI to run flow in a Python process:
+# prefect run -p core/evolving_etl_2/level_3.py
