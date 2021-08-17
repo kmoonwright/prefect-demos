7a024e457b7add70170f78c6a414cf30e3af2984
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-08-17 13:58:22 -0700

    Add comments and CLI command

--- core/evolving_etl_2/level_1a.py
+++ core/evolving_etl_2/level_1a.py
@@ -1,6 +1,7 @@
 from random import randrange
 from prefect import task, Flow
 
+# Task Definitions
 @task
 def extract():
     return randrange(1, 100)
@@ -13,7 +14,11 @@ def transform(data):
 def load(data):
     print(f"\nHere's your data: {data}")
 
+# Flow Definition for task dependencies
 with Flow("Evolving ETL") as flow:
     e = extract()
     t = transform(e)
     l = load(t)
+
+# Rather than flow.run(), utilize the CLI to run flow in a Python process:
+# prefect run -p core/evolving_etl_2/level_1.py
