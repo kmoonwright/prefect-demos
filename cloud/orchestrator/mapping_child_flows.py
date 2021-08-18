da1f5c423c5e9afdf9219cf60ab812e0ae5a8d43
Kyle Moon-Wright <kmoonwright@outlook.com>
2021-08-17 18:20:26 -0700

    Add flow that maps to create child flows

---
+++ cloud/orchestrator/mapping_child_flows.py
@@ -0,0 +1,20 @@
+# From Chris/Nicholas
+from prefect import task, Flow, Parameter, unmapped
+from prefect.tasks import StartFlowRun
+from typing import List
+
+@task
+def taskA() -> List[int]: 
+  return results
+
+@task
+def create_parameter_payload(result, param):
+    return {'blah': param, 'resultId': result}
+
+with Flow("Mapping Parameters Flow") as flow:
+    my_results = taskA()
+    flow_run_task = StartFlowRun()
+    param = Parameter("My Parameter Value")
+
+    params = create_parameter_payload.map(param=unmapped(param), result=my_results)
+    flow_run_task.map(flow_name=unmapped('DownstreamFlow'), parameters=params)
